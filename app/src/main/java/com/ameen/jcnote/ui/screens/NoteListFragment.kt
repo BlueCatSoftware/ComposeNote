b34e676bdd43ec4b9c5b234b6951e875b8b6b1ce
BlueCat Software <73719772+BlueCatSoftware@users.noreply.github.com>
2022-08-01 02:46:31 +0100

    feat: removed redundant package

--- app/src/main/java/com/ameen/jcnote/ui/screens/NoteListFragment.kt
+++
@@ -1,6 +0,0 @@
-package com.ameen.jcnote.ui.screens
-
-import androidx.fragment.app.Fragment
-
-class NoteListFragment : Fragment() {
-}
--- app/src/main/java/com/ameen/jcnote/ui/screens/ViewNoteFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.ameen.jcnote.ui.screens
-
-import androidx.fragment.app.Fragment
-
-
-class ViewNoteFragment : Fragment() {
-
-}
